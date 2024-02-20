766e4e8c3e8185620eb0805344aa14267a3164df
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2024-02-19 22:26:41 -0300

    Ambiente de desenvolvimento

---
+++ src/main/java/com/itb/lip2/academicologininf3bn/AcademicoLoginInf3bnApplication.java
@@ -0,0 +1,13 @@
+package com.itb.lip2.academicologininf3bn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicoLoginInf3bnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicoLoginInf3bnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/lip2/academicologininf3bn/AcademicoLoginInf3bnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.lip2.academicologininf3bn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicoLoginInf3bnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
